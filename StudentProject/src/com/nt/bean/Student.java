a417f3147cda182920554329fb3b3bafa35c13b7
HP <HP@LAPTOP-5TSFA7HT>
2023-07-31 07:17:52 +0530

    yyyyy

--- StudentProject/src/com/nt/bean/Student.java
+++ StudentProject/src/com/nt/bean/Student.java
@@ -7,6 +7,7 @@ public class Student {
 	public static void main(String[] args) {
 		System.out.println(new Date());
 		System.out.println("modified by tl 1234");
+		System.out.println("after igonore");
 		
 	}
 
